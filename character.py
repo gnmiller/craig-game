23d5ea8c8d399af9c1c66b73baf42b7d1c577e5d
GM <gnmiller@mtu.edu>
2023-08-04 16:29:43 -0400

    initial

---
+++ character.py
@@ -0,0 +1,412 @@
+import item
+import math
+
+
+class Stats:
+    """Wrapper for character stats.
+
+    Attributes:
+        strength        Character's strength
+        agility         Character's agility
+        intellect       Character's intellect
+        charisma        Character's charisma
+        constitution    Character's constitution
+        luck            Character's luck
+        inventory       The character's inventory (NYI)
+    """
+    def __init__(self, strength=0, agility=0, intellect=0,
+                 charisma=0, con=0, luck=0) -> None:
+        """Create a new stat block for a `character.Character`
+
+        Parameters
+        ----------
+        strength    Starting strength value.
+        agility    Starting agility value.
+        intellect    Starting intellect value.
+        charisma    Starting charisma value.
+        constitution    Starting constitution value.
+        luck    Starting luck value."""
+        self._strength = strength
+        self._agility = agility
+        self._intellect = intellect
+        self._charisma = charisma
+        self._constitution = con
+        self._luck = luck
+        self._index = 0
+        return
+
+    #  Strength Code
+    @property
+    def strength(self) -> int:
+        return self._strength
+
+    @strength.setter
+    def strength(self, val: int) -> None:
+        self._strength = val
+
+    @strength.deleter
+    def strength(self) -> None:
+        del self._strength
+    #  End Strength
+
+    #  Agility Code
+    @property
+    def strength(self) -> int:
+        return self._agility
+
+    @strength.setter
+    def strength(self, val: int) -> None:
+        self._agility = val
+
+    @strength.deleter
+    def strength(self) -> None:
+        del self._agility
+    #  End Agility
+
+    #  Intellect Code
+    @property
+    def strength(self) -> int:
+        return self._intellect
+
+    @strength.setter
+    def strength(self, val: int) -> None:
+        self._intellect = val
+
+    @strength.deleter
+    def strength(self) -> None:
+        del self._intellect
+    #  End Intellect
+
+    # Charisma Code
+    @property
+    def strength(self) -> int:
+        return self._charisma
+
+    @strength.setter
+    def strength(self, val: int) -> None:
+        self._charisma = val
+
+    @strength.deleter
+    def strength(self) -> None:
+        del self._charisma
+    #  End Charisma
+
+    #  Constitution Code
+    @property
+    def strength(self) -> int:
+        return self._constitution
+
+    @strength.setter
+    def strength(self, val: int) -> None:
+        self._constitution = val
+
+    @strength.deleter
+    def strength(self) -> None:
+        del self._constitution
+    #  End Constitution
+
+    #  Luck Code
+    @property
+    def strength(self) -> int:
+        return self._luck
+
+    @strength.setter
+    def strength(self, val: int) -> None:
+        self._luck = val
+
+    @strength.deleter
+    def strength(self) -> None:
+        del self._luck
+    #  End Luck
+
+    def __str__(self):
+        """Generate a dict of stat_name: stat_value and return it as a string."""
+        rstats = {}
+        for k, v in vars(self).items():
+            if "index" in k:
+                continue
+            rstats[k[1:len(k)]] = v
+        return str(rstats)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """Iterate over the stats of this object.
+
+        Iterates over vars(self). Using the data provided by vars()
+        the iterator tries to lookup the key name (stat name), and
+        then pulls the corresponding value from vars(). Meant to be
+        scalable for new stats in the future.
+        May have unexpected results if vars() does not provide a consistent
+        ordering of the objects."""
+        if self._index < len(vars(self).keys()):
+            key = list(vars(self).keys())[self._index]
+            if "index" in key:
+                self._index += 1
+                return next(self)
+            value = vars(self)[key]
+            self._index += 1
+            return key[1:len(key)], value
+        else:
+            raise StopIteration
+
+
+class Gear:
+    """Container for a character's equipped items.
+
+    This is effectively a container for `item.Equipment` objects. Each
+    property is intended to hold a single `item.Equipment` with an
+    `item.Slot` corresponding to the attribute name.
+
+    Attributes:
+        head Helmet equipment
+        chest Chest armor
+        arms Bracer/arm armor
+        legs Leg armor
+        hands Gloves
+        rings A list of two rings.
+        trinket A special trinket (ooo fancy)
+        weapon A weapon
+        oh Another weapon, or maybe a shield?
+        """
+    def __init__(self):
+        """Create a new Gear container for a character.
+
+        You start naked! No default values are assigned by the constructor.
+        Except in the case of rings which is assigned an empty list."""
+        self._head = None
+        self._chest = None
+        self._arms = None
+        self._legs = None
+        self._hands = None
+        self._rings = []
+        self._trinket = None
+        self._weapon = None  # Main-hand weapon
+        self._oh = None  # Off-hand weapon
+
+    # Define the property getters and setters
+    @property
+    def head(self) -> item.Equipment:
+        return self._head
+
+    @head.setter
+    def head(self, item: item.Equipment):
+        self._head = item
+
+    @property
+    def chest(self):
+        return self._chest
+
+    @chest.setter
+    def chest(self, item: item.Equipment):
+        self._chest = item
+
+    @property
+    def arms(self) -> item.Equipment:
+        return self._arms
+
+    @arms.setter
+    def arms(self, item: item.Equipment):
+        self._arms = item
+
+    @property
+    def legs(self) -> item.Equipment:
+        return self._legs
+
+    @legs.setter
+    def legs(self, item: item.Equipment):
+        self._legs = item
+
+    @property
+    def hands(self) -> item.Equipment:
+        return self._hands
+
+    @hands.setter
+    def hands(self, item: item.Equipment):
+        self._hands = item
+
+    @property
+    def rings(self) -> item.Equipment:
+        return self._rings
+
+    @rings.setter
+    def ring1(self, item: item.Equipment, rslot: int = 0):
+        if rslot == 0 or rslot == 1:
+            self._rings[rslot] = item
+        else:
+            raise ValueError("you can only wear 2 rings bozo")
+
+    @property
+    def trinket(self) -> item.Equipment:
+        return self._trinket
+
+    @trinket.setter
+    def trinket(self, item: item.Equipment):
+        self._trinket = item
+
+    @property
+    def weapon(self) -> item.Equipment:
+        return self._weapon
+
+    @weapon.setter
+    def weapon(self, item: item.Equipment):
+        self._weapon = item
+
+    @property
+    def oh(self) -> item.Equipment:
+        return self._oh
+
+    @oh.setter
+    def oh(self, item: item.Equipment):
+        self._oh = item
+
+
+class Level:
+    """A container representing a character's level.
+
+    An object containing the character's current level
+    and experience amount.
+
+    Attributes:
+        cur_level   The character's current level as an integer
+        exp         The character's experience count as an integer
+    """
+    def __init__(self, cur_level: int = 0, exp: int = 0):
+        """Create a new Level object for a character.
+
+        Characters start at level 0 with 0 experience by default.
+
+        Parameters
+        ----------
+            cur_level   The character's current level
+            exp         The character's current experience amount"""
+        self._cur_level = cur_level
+        self._exp = exp
+
+    def _get_next(self):
+        """Returns the exp needed for the next level for this `character.Level"""
+        t = 100
+        b = .15
+        x = self.cur_level
+        y = t * (1 + b) ** x
+        return math.ceil(y)
+
+    @property
+    def cur_level(self) -> int:
+        return self._cur_level
+
+    @cur_level.setter
+    def cur_level(self, value: int):
+        if isinstance(value, int) and value >= 1:
+            self._cur_level = value
+        else:
+            raise ValueError("Current level must be an integer >= 1.")
+
+    @property
+    def exp(self) -> int:
+        return self._exp
+
+    @exp.setter
+    def exp(self, value: int):
+        if isinstance(value, int) and value >= 0:
+            self._exp = value
+        else:
+            raise ValueError("Experience points (exp) must be non-negative.")
+
+    @property
+    def exp_to_level(self) -> int:
+        return self._exp_to_level
+
+    @exp_to_level.setter
+    def exp_to_level(self, value: int):
+        if isinstance(value, int) and value > 0:
+            self._exp_to_level = value
+        else:
+            raise ValueError("Exp to level must be a positive integer > 0.")
+
+
+class Character:
+    """The hero of the story! Contains all the important information.
+
+    The Character class is designed to gather up all the other important
+    information into one easy to reference location. The inention is to be
+    able to snag all the information out of the DB create a `character.Gear`
+    and a `character.Stat` then hand it all off to this object's constructor.
+
+    Attributes:
+        name        Who are you?
+        level       `character.Level` for this character
+        stats       `character.Stats` for this character
+        gear        `Character.Gear` for this character
+        bt_class    class (NYI)
+        """
+    def __init__(self, name: str,
+                 level: Level = Level(0, 0),
+                 stat_block: Stats = Stats(),
+                 gear_block: Gear = Gear(),
+                 bt_class: str = "none"):
+        self._level = level
+        self._name = name
+        self._stats = stat_block
+        self._gear = gear_block
+        self._bt_class = bt_class
+        return
+
+    #  Level
+    @property
+    def level(self) -> int:
+        return self._level
+
+    @level.setter
+    def level(self, value):
+        if isinstance(value, int) and value > 0:
+            self._level = value
+        else:
+            raise ValueError("Level must be a positive integer.")
+    #  End Level
+
+    #  Name
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        if isinstance(value, str):
+            self._name = value
+        else:
+            raise ValueError("Name must be a string.")
+    #  End Name
+
+    #  Stats
+    @property
+    def stats(self) -> Stats:
+        return self._stats
+
+    @stats.setter
+    def stats(self, value: Gear):
+        if isinstance(value, Gear):
+            self._stats = value
+        else:
+            raise ValueError("Stats must be a Gear.")
+    #  End Stats
+
+    #  Gear
+    @property
+    def gear(self) -> Gear:
+        return self._gear
+
+    @gear.setter
+    def gear(self, value: Gear):
+        if isinstance(value, Gear):
+            self._gear = value
+        else:
+            raise ValueError("Gear must be a Gear.")
+    #  End Gear
+
+    def __str__(self) -> str:
+        return f"Character: {self._name}\n" \
+               f"Class: {self._bt_class}\n"\
+               f"Level: {self._level}\n" \
+               f"Stats: {self._stats}"
---
+++ item.py
@@ -0,0 +1,346 @@
+class Slot:
+    """A super class representing the possible gear slots for a character.
+
+    A wrapper class for some basic data on slots for character equipment.
+    Generally meant to be used as a super class for a specific equipment
+    slot.
+    slot_id is a unique ID for each possible slot on a character,
+    intended to be scalable and modifiable as needed in the future.
+
+    Attributes:
+        slot    The text representation of the slot. ex slot 0 is 'head'"""
+
+    #  im sure theres a better way to maintain this
+    slots = {
+        0: 'head',
+        1: 'chest',
+        2: 'arms',
+        3: 'legs',
+        4: 'hands',
+        5: 'finger',
+        6: 'trinket',
+        7: 'weapon',
+        8: 'offhand',
+    }
+    rev_slots = {
+        'head': 0,
+        'chest': 1,
+        'arms': 2,
+        'legs': 3,
+        'hands': 4,
+        'finger': 5,
+        'trinket': 6,
+        'weapon': 7,
+        'offhand': 8,
+    }
+
+    def __init__(self, slot_id=0):
+        """Create a Slot.
+
+        Parameters
+        ----------
+        slot_id An integer that represents a slot in Slot.slots
+
+        Raises
+        ----------
+        ValueError Raises a ValueError if an int x<0 or x>len(slots), or
+            when a str is passed but the str is not in the allowed list.
+        TypeError   Raises a type error if a type other than int or str is
+            passed.
+        """
+        if isinstance(slot_id, str):
+            if slot_id in self.rev_slots.keys():
+                self.slot_id = self.rev_slots[slot_id]
+                return
+            else:
+                raise ValueError("please provide a valid str")
+        if not isinstance(slot_id, int):
+            raise TypeError("please provide an int or valid str")
+        if slot_id < 0 or slot_id > len(self.slots):
+            raise ValueError("Please provide an allowed value "
+                             f"[0 <= {slot_id} <={len(self.slots)}]")
+        else:
+            self.slot_id = slot_id
+            return
+
+    @property
+    def slot(self, what: int):
+        return self.slots[what]
+
+    @slot.setter
+    def slot(self, slot_id):
+        self.slot_id = slot_id
+
+    def __str__(self) -> str:
+        return self.slots[self.slot_id]
+
+
+class Material:
+    """Material type used by items (weapons and armor).
+
+    This class is intended to be used by the `item.Item` class to store
+    details on the item's material and tier. Allowing for adding
+    new properties in the future.
+
+    Attributes:
+        material_type   The type of the material. A string representing
+            the material name.
+        material_tier   A tier representing how powerful the material
+            is compared to others"""
+
+    def __init__(self, material_type: str = None,
+                 material_tier: float = 0,
+                 *args, **kwargs):
+        """Construct a new Material from the provided material type and tier
+
+        Creates a new object based on the tier and type provided. Currently
+        type is a string of some relevant material like 'iron'. Tier is
+        intended to represent the relative power of two different materials.
+
+        Parameters
+        ----------
+        material_type   A string representation of the materials type.
+        material_tier   An integer representing the relative power of the
+            material.
+        *args           If "all" is provided all slots available in Slot.slots
+            are added. If a Slot is passed then the Slot.slot_id is added,
+            finally if any integers are passed it is appended directly.
+        **kwargs        If "slots" is passed as a kwarg the valid list is
+            copied into the _valid variable. Any arguments passed in *args
+            are ignored in this case.
+        """
+        self._material_type = material_type
+        self._material_tier = material_tier
+        self._valid = []
+        try:
+            kwargs['slots']
+        except KeyError:
+            kwargs['slots'] = None
+        if kwargs['slots'] is not None\
+                and isinstance(kwargs['slots'], list):
+            self._valid = kwargs['slots'].copy()
+        else:
+            for arg in args:
+                if isinstance(arg, str) and "all" in str:
+                    for i in range(0, len(Slot.slots)):
+                        self._valid.append(i)
+                if isinstance(arg, Slot):
+                    self._valid.append(Slot.slot_id)
+                if isinstance(arg, list):
+                    for e in arg:
+                        if isinstance(e, Slot):
+                            self._valid.append(e)
+                if isinstance(arg, int) and arg in Slot.slots.values():
+                    self._valid.append(arg)
+            temp = []
+            [temp.append(x) for x in self._valid if x not in temp]
+            self._valid = temp.copy()
+
+    @property
+    def material_type(self) -> str:
+        return self._material_type
+
+    @material_type.setter
+    def material_type(self, value: str):
+        if isinstance(value, str):
+            self._material_type = value
+        else:
+            raise ValueError("Material type must be a string.")
+
+    @property
+    def material_tier(self) -> int:
+        return self._material_tier
+
+    @material_tier.setter
+    def material_tier(self, value: int):
+        if isinstance(value, int) and value >= 0:
+            self._material_tier = value
+        else:
+            raise ValueError("Material tier must be a non-negative integer.")
+
+    def __str__(self) -> str:
+        out = f"Material Type: {self._material_type}, " \
+               f"Material Tier: {self._material_tier}, " \
+               f"Valid for: "
+        for m in self._valid:
+            out += str(m)+", "
+        return out[0:len(out)-2]
+
+
+#  TODO create an Item superclass
+class Equipment:
+    """Equipment super class
+
+    The super class for all equipment type items. Containing the properties
+        shared for all equipment. Generally should inherited by a specific
+        equipment type (based on an `item.Slot`)
+
+    Attributes:
+        name    A string representing the name of the equipment.
+                    Typically formed by combining the material and slot
+        item_id A unique ID assigned to this equipment.
+        slot    An `item.Slot` for this equipment
+        material An `item.Materia`l for this equipment
+        strength The strength bonus this equipment grants
+        agility The agility bonus this equipment grants
+        intellect The intellect bonus this equipment grants
+        charisma The charisma bonus this equipment grants
+        constitution The constitution bonus this equipment grants
+        luck The luck bonus this equipment grants"""
+    def __init__(self, *args, **kwargs):
+        self._name = kwargs['name']
+        self._item_id = kwargs['item_id']
+        self._slot = kwargs['slot']
+        self._b_strength = kwargs['strength']
+        self._b_agility = kwargs['agility']
+        self._b_intellect = kwargs['intellect']
+        self._b_charisma = kwargs['charisma']
+        self._b_constitution = kwargs['constitution']
+        self._b_luck = kwargs['luck']
+        self._material = kwargs['material']
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        self._name = value
+
+    @property
+    def item_id(self) -> int:
+        return self._item_id
+
+    @item_id.setter
+    def item_id(self, value: int):
+        self._item_id = value
+
+    @property
+    def slot(self) -> str:
+        return self._slot
+
+    @slot.setter
+    def slot(self, value: str):
+        self._slot = value
+
+    @property
+    def strength(self) -> int:
+        return self._b_strength
+
+    @strength.setter
+    def strength(self, value: int):
+        self._b_strength = value
+
+    @property
+    def agility(self) -> int:
+        return self._b_agility
+
+    @agility.setter
+    def agility(self, value: int):
+        self._b_agility = value
+
+    @property
+    def intellect(self) -> int:
+        return self._b_intellect
+
+    @intellect.setter
+    def intellect(self, value: int):
+        self._b_intellect = value
+
+    @property
+    def charisma(self) -> int:
+        return self._b_charisma
+
+    @charisma.setter
+    def charisma(self, value: int):
+        self._b_charisma = value
+
+    @property
+    def constitution(self) -> int:
+        return self._b_constitution
+
+    @constitution.setter
+    def constitution(self, value: int):
+        self._b_constitution = value
+
+    @property
+    def luck(self) -> int:
+        return self._b_luck
+
+    @luck.setter
+    def luck(self, value: int):
+        self._b_luck = value
+
+    @property
+    def material(self) -> Material:
+        return self._material
+
+    @material.setter
+    def material(self, new_material: Material = Material("wood", 0)):
+        self._material = new_material
+
+    def __str__(self) -> str:
+        return (f"Item: {self._name} (ID: {self._item_id}), "
+                f"Slot: {self._slot},"
+                f"Material: {self.material.material_type}, "
+                f"({self.material.material_tier}), "
+                f"Strength: {self._b_strength}, "
+                f"Agility: {self._b_agility}, "
+                f"Intellect: {self._b_intellect}, "
+                f"Charisma: {self._b_charisma}, "
+                f"Constitution: {self._b_constitution}, "
+                f"Luck: {self._b_luck}")
+
+
+class Head(Equipment):
+    def __init__(self, **kwargs):
+        self._slot = Slot(0)
+        super(Weapon, self).__init__(**kwargs)
+
+
+class Chest(Equipment):
+    def __init__(self, **kwargs):
+        self._slot = Slot(1)
+        super(Weapon, self).__init__(**kwargs)
+
+
+class Arms(Equipment):
+    def __init__(self, **kwargs):
+        self._slot = Slot(2)
+        super(Weapon, self).__init__(**kwargs)
+
+
+class Legs(Equipment):
+    def __init__(self, **kwargs):
+        self._slot = Slot(3)
+        super(Weapon, self).__init__(**kwargs)
+
+
+class Hands(Equipment):
+    def __init__(self, **kwargs):
+        self._slot = Slot(4)
+        super(Weapon, self).__init__(**kwargs)
+
+
+class Ring(Equipment):
+    def __init__(self, **kwargs):
+        self._slot = Slot(5)
+        super(Weapon, self).__init__(**kwargs)
+
+
+class Trinket(Equipment):
+    def __init__(self, **kwargs):
+        self._slot = Slot(6)
+        super(Weapon, self).__init__(**kwargs)
+
+
+class Weapon(Equipment):
+    def __init__(self, **kwargs):
+        self._slot = Slot(7)
+        super(Weapon, self).__init__(**kwargs)
+
+
+class OffHand(Equipment):
+    def __init__(self, **kwargs):
+        self._slot = Slot(8)
+        super(Weapon, self).__init__(**kwargs)
