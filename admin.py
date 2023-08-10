4209d14bf8c92ac27c28bf97795a30b4ae245768
GM <gnmiller@mtu.edu>
2023-08-10 13:04:42 -0400

    adding some commands for testing files while bot is running

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import shutil
 import config
+import os
 
 
 class admin_Commands(commands.Cog):
@@ -72,3 +73,34 @@ class admin_Commands(commands.Cog):
         except FileNotFoundError as e:
             await ctx.respond("could not delete files check disk")
             raise FileNotFoundError(f"could not find {path} -- {e}")
+
+    @commands.slash_command(
+        description="Initialize the data directory for the game.",
+        help="Init game data dir for testing.",
+        brief="Init game data dir for testing.",
+        hidden=True,
+        name='reset'
+    )
+    @commands.is_owner()
+    async def _reset(self, ctx):
+        try:
+            data_dir = f"./{config.data['data_dir']}"
+            if not os.path.isdir(data_dir):
+                os.makedirs(data_dir)
+            char_files_dir = f"{data_dir}/{config.data['char_dir']}"
+            if not os.path.isdir(char_files_dir):
+                os.makedirs(char_files_dir)
+            active_files_dir = f"{data_dir}/{config.data['active_dir']}"
+            if not os.path.isdir(active_files_dir):
+                os.makedirs(active_files_dir)
+            await ctx.respond("```Initialized game data.```")
+        except FileExistsError as e:
+            raise FileExistsError(f"Failed to initialize game data files. {e}")
+
+    @commands.slash_command(
+        hidden=True,
+        name='coin'
+    )
+    @commands.is_owner()
+    async def _coin(self, ctx):
+        await ctx.respond("\N{coin}")
